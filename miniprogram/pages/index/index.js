78eaacc9503516e64ad07f84574c52812458343a
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-10 15:40:02 +0800

    plugin

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,8 +7,22 @@ Page({
   },
 
   onLoad() {
-    this.xx = new plugin.BluetoothPrinter(wx)
-    this.cpcl = new plugin.PrintCPCL()
+    this.plugin = plugin
+    this.printer = new plugin.BluetoothPrinter(wx)
+    this.printer.registeredDevices = ['DP-HT303-5402']
+  },
+
+  printPos() {
+    const pos = new plugin.PrintPOS()
+    pos.text_big('创印智能 RRRRRRR')
+    const data = pos.render()
+    console.debug('处理后的数据：', data)
+
+    this.printer.getState({
+      success: (res) => {
+        this.printer.writeValue(data)
+      }
+    })
   }
 
 })

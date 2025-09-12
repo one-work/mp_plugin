f1835c27f30f97c94545d434e9c5f65fffe2dbeb
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-12 21:32:21 +0800

    print demo logic

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -10,19 +10,6 @@ Page({
     this.plugin = plugin
     this.printer = new plugin.BluetoothPrinter(wx)
     this.printer.registeredDevices = ['DP-HT303-5402']
-  },
-
-  printPos() {
-    const pos = new plugin.PrintPOS()
-    pos.text_big('创印智能 RRRRRRR')
-    const data = pos.render()
-    console.debug('处理后的数据：', data)
-
-    this.printer.getState({
-      success: (res) => {
-        this.printer.writeValue(data)
-      }
-    })
   }
 
 })

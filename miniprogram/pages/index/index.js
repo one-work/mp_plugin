56f01910455cf5620efdf4b1bf116d0348469fe7
qinmingyuan <mingyuan0715@foxmail.com>
2026-04-13 21:10:38 +0800

    suport trigger custom event

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -8,6 +8,18 @@ Page({
   onLoad() {
     this.printer = new plugin.BluetoothPrinter(wx, this)
     this.printer.registeredDevices = [this.data.name]
+  },
+
+  selected(e) {
+    console.log(e)
+    this.setData({
+      name: e.detail.name,
+      deviceId: e.detail.deviceId
+    })
+  },
+
+  unselected(e) {
+    console.log(e)
   }
 
 })
